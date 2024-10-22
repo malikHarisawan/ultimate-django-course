6635924fe86b14dbc9458c00ea6c7c59e33101eb
malikHarisawan <malikharisawan06@gmail.com>
2024-10-22 17:54:22 +0500

    inital

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from store.models import Product,Order,OrderItem,Customer
+from django.db.models.aggregates import Count,Max,Min,Avg,Sum
 
-
-def calculate():
-    x = 1
-    y = 2
-    return x
-
-
+from django.contrib.contenttypes.models import ContentType
+from tags.models import TaggedItem
 def say_hello(request):
-    x = calculate()
-    return render(request, 'hello.html', {'name': 'Mosh'})
+    #products = OrderItem.objects.values('product__title').order_by('product__title').distinct()
+    #products = Order.objects.select_related('customer').order_by('-placed_at')[:5]
+    #products = Product.objects.values('title', 'collection__title').order_by('-collection__title').filter(unit_price__lte=30)
+    #products = Order.objects.aggregate(count=Count('id'))
+    #products = Order.objects.filter(customer__id=1).aggregate(total = Count('id'))
+    #products = OrderItem.objects.filter(product__id=1).aggregate(total = Sum('quantity'))
+    #products = Product.objects.filter(collection__id=3).aggregate(max = Max('unit_price'), min = Min('unit_price'),avg = Avg('unit_price'))
+    #products = Customer.objects.annotate(customer_orders = Count('order'))
+    ct = ContentType.objects.get_for_model(Product)
+    products = TaggedItem.objects.select_related('tag').filter(content_type = ct, object_id = 2)
+    return render(request, 'hello.html', {'name': 'Mosh' ,'products': list(products)})
--- store/models.py
+++ store/models.py
@@ -39,7 +39,7 @@ class Customer(models.Model):
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
     membership = models.CharField(
-        max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+    max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
 
 
 class Order(models.Model):
@@ -54,7 +54,7 @@ class Order(models.Model):
 
     placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(
-        max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
+    max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
 
 
